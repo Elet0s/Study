d973f608003f8fd9e044588e041f5832870f92d5
Eletos <lthlovelyg@naver.com>
2023-04-03 18:50:57 +0900

    이관형 : 버블정렬 완료

--- 2023/0207/0313/BubleSort.cpp
+++ 2023/0207/0313/BubleSort.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include<iostream>
 
-int BubleSort(int array[], size_t array_len)//array_len는 배열 길이
+void BubleSort(int array[], size_t array_len)//array_len는 배열 길이
 {
 	switch (array_len)
 	{
@@ -13,22 +14,35 @@ int BubleSort(int array[], size_t array_len)//array_len는 배열 길이
 	default:
 		for (size_t i = 0; i < array_len; ++i)
 		{
-			for (size_t j = 0; j < array_len; ++j)
+			for (size_t j = 0; j < array_len-1; ++j)
 			{
-
+				 if (array[j] > array[j + 1])
+				{
+					std::swap(array[j], array[j + 1]);
+				}
 			}
 		}
 	}
 }
 
 
-int main()
+void main()
 {
    int A[5] = { 0 ,5, 3,4,1 };
 
-   int B[1] = { 0 };
+   int B[10] = { 21,44,54,12,3,5,8,99,1,1 };
 
    BubleSort(A, 5);
-   BubleSort(B, 1);
-    return 0;
+
+   for (size_t x = 0; x < 5; ++x)
+   {
+	   std::cout << A[x] << std::endl;
+   }
+
+   BubleSort(B, 10);
+
+   for (size_t x = 0; x < 10; ++x)
+   {
+	   std::cout << B[x] << std::endl;
+   }
 }
